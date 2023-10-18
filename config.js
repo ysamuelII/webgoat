f798f2dd5cef78966a6d4cc097fe0c403ae32f8e
ysamuelII <84854938+ysamuelII@users.noreply.github.com>
2023-10-18 17:11:23 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const ACCESS_KEY = process.env.ACCESS_KEY;
+
+export const GRAPHQL_ENDPOINT = process.env.GRAPHQL_ENDPOINT;
+export const LOGIN_VARIABLES = {
+  login: 'ashwin',
+  password: 'dakka',
+  role: 'Internal',
+};
